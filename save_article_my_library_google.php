8751e93232334629b981fd46330c44dd9d3e4fa7
Mauricio Manoel <mauriciomanoel@gmail.com>
2018-01-28 15:57:41 -0300

    update

--- save_article_my_library_google.php
+++ save_article_my_library_google.php
@@ -19,7 +19,6 @@
                 if ($node->getAttribute( 'class' ) == "gs_or_ggsm") {
                     $child = $node->firstChild;
                     $link_pdf = trim($child->getAttribute( 'href' ));
-                    // echo "<pre>"; var_dump($child->getAttribute( 'href' ));
                 }
                 if ($node->getAttribute( 'class' ) == "gs_ri") {
                     $childsDiv = $node->childNodes;
@@ -37,26 +36,8 @@
                             $cited_by = $matches[0];
                             break;
                         }
-                        // if ($child->getAttribute( 'class' ) == "gs_fl") {
-                        //     $childsA = $node->childNodes;
-                        //     $nodeTitle = $childsA->item(5);
-                        //     if (strpos(trim($nodeTitle->textContent), "Citado por") !== false) {
-                        //         $cited_by = trim(str_replace("Citado por","",trim($nodeTitle->textContent)));
-                        //         var_dump( $cited_by); exit;
-                        //     }
-
-                        // }
                     }
                 }
-
-                // if ($node->getAttribute( 'class' ) == "gs_fl") {
-                //     $childs = $node->childNodes;
-                //     $nodeTitle = $childs->item(5);
-                //     if (strpos(trim($nodeTitle->textContent), "Citado por") !== false) {
-                //         $cited_by = trim(str_replace("Citado por","",trim($nodeTitle->textContent)));
-                //         var_dump( $cited_by);
-                //     } 
-                // }                
             }
             if (!empty($data_cid) && !empty($title_article) && !empty($link_article)) {
                 // var_dump($cited_by); exit;
@@ -70,8 +51,9 @@
         }
         
         if (!empty($content)) {
-            var_dump( $content);
-            file_put_contents($file, $content);
+            $oldContent = @file_get_contents($file);
+            $newContent = $oldContent . $content;
+            file_put_contents($file, $newContent);
         }
     
     }
@@ -83,7 +65,7 @@
         // @$dom->loadHTML($html);
         $dom->loadHTML(mb_convert_encoding($html, 'HTML-ENTITIES', 'UTF-8'));
         $dom->preserveWhiteSpace = true;
-        save_data_key($dom, $file);
+        save_data_key($dom, $file); // save data local
         foreach ($dom->getElementsByTagName('div') as $node) {
             if ($node->hasAttribute( 'data-cid' )) {
                 $data_cid = $node->getAttribute( 'data-cid' );
@@ -95,16 +77,16 @@
         }       
     }
 
-    $page = 270;
-    $file = $page . "_Internet of Things_Medical.csv";
+    $page = 0;
+    $file = "google_scholar_Internet_of_Things_Medical.csv";
 
-    libxml_use_internal_errors(true) && libxml_clear_errors(); // for html5
-        $dom = new DOMDocument('1.0', 'UTF-8');
-        $html = file_get_contents("google5.html");
-        $dom->loadHTML(mb_convert_encoding($html, 'HTML-ENTITIES', 'UTF-8'));
-        save_data_key($dom, $file);
+    // libxml_use_internal_errors(true) && libxml_clear_errors(); // for html5
+    // $dom = new DOMDocument('1.0', 'UTF-8');
+    // $html = file_get_contents("google5.html");
+    // $dom->loadHTML(mb_convert_encoding($html, 'HTML-ENTITIES', 'UTF-8'));
+    // save_data_key($dom, $file);
     
-        exit;
+    exit;
     $url = "https://scholar.google.com.br/scholar?start=" . $page . "&q=%22Internet+of+Things%3B+Medical%22&hl=pt-BR&as_sdt=0,5";
     progress_google($url, $file);
 ?>
