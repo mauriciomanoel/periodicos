a6f7294672d7f5786a4d41f0b059a990c60fb1dd
Mauricio Manoel <mauriciomanoel@gmail.com>
2018-02-04 00:37:53 -0300

    update

--- config.php
+++ config.php
@@ -1,7 +1,6 @@
 <?php
-    define('COOKIE_CAPES', 'JSESSIONID=386E9833FC77B01379075A78E5A22265; sto-id-%3FSaaS-A_prod%3FPMTNA03.prod.primo.1701=HNHIBMAK; JSESSIONID=A5BB1A3C794E266BDF40001D7DE7016A; PRIMO_RT=s=1517685755298&r=http%3A%2F%2Frnp-primo.hosted.exlibrisgroup.com%2Fprimo_library%2Flibweb%2Faction%2Fsearch.do%3Fct%3DNext%2BPage%26pag%3Dnxt%26indx%3D11%26pageNumberComingFrom%3D4%26frbg%3D%26indx%3D31%26fn%3Dsearch%26dscnt%3D0%26scp.scps%3Dscope%253A(%2522CAPES%2522)%252CEbscoLocalCAPES%252Cprimo_central_multiple_fe%26vid%3DCAPES_V1%26mode%3DBasic%26ct%3DNext%2520Page%26srt%3Drank%26tab%3Ddefault_tab%26dum%3Dtrue%26vl(freeText0)%3D%2522Internet%2520of%2520Things%2522%2520AND%2520Medical%26dstmp%3D1517685531551&p=QWERTY');
-    define('USER_AGENT_WINDOWS','Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:58.0) Gecko/20100101 Firefox/58.0');
-    define('USER_AGENT_MACOS','Mozilla/5.0 (Macintosh; Intel Mac OS X 10.13; rv:57.0) Gecko/20100101 Firefox/57.0');
+    define('COOKIE_CAPES', 'JSESSIONID=87347C24AE334CF33F3FF82CBC3A0A1A; sto-id-%3FSaaS-A_prod%3FPMTNA03.prod.primo.1701=HNHIBMAK; PRIMO_RT=s=1517712151156&r=http%3A%2F%2Frnp-primo.hosted.exlibrisgroup.com%2Fprimo_library%2Flibweb%2Faction%2Fbasket.do%3Ffn%3Ddisplay%26fromUserArea%3Dtrue%26vid%3DCAPES%26fromPreferences%3Dfalse%26fromLink%3DgotoeShelfUI&p=QWERTY');
+    define('USER_AGENT', $_SERVER["HTTP_USER_AGENT"]);
     define('COOKIE_GOOGLE', 'GSP=A=YTLx9g:CPTS=1517668153:LM=1517668153:S=GpNlOJK1VaFBRA-t; NID=123=lipMm-n_k8E4D1WzQgeUe4puuSd17nR2dtXj9mnkaP5lQG-eO466aw1b3YgnR5Wf7BD7oKb3qrpHq3o81GZWcdQfgZsGydEwRjqz3p8kxeLWg7yysf0VCCq0If0zNtDN');
     define('QUERY', '%22Internet%20of%20Things%22%20AND%20Medical');
     define('XSRF_GOOGLE','AMstHGQAAAAAWnMRp0OEUK1-sGxeA7FK3mM_6CqbxAo8');
